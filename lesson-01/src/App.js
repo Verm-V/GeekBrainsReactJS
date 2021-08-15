7e75370a3e639e017ae593eb1a7098bd66ea7d91
Verm-V <verm-v@yandex.ru>
2021-08-16 00:34:56 +0600

    Удалены лишние файлы из проекта. Добавлен компонент Message в который по нажатию кнопки выводится сообщение.

--- lesson-01/src/App.js
+++ lesson-01/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Message from "./Message";
 
 function App() {
+
+  const [inputText, setInputText] = useState('')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Hello, humans!</div>
+      <button onClick={(e) => setInputText("All your base are belong to us!")}>Press the button</button>
+      <Message textToShow={inputText}/>
     </div>
   );
 }
